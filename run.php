5851a3017885b33761e3a004862524e87927b856
Casey McLaughlin <caseyamcl@gmail.com>
2012-07-24 14:34:00 -0400

    Added web output option.

--- run.php
+++ run.php
@@ -19,38 +19,47 @@ main();
  * Main Execution Function
  */
 function main() {
-    
-  //Load Config
-  $config = array();
-  if ( ! @include('config.local.php')) {
-    die("Error: Setup your configuration file (see config.sample.php for instructions)!");
-  }
 
-  $api_user = $config['api_user'];
-  $api_key  = $config['api_key'];
-  $api_url  = $config['api_url'];
-  
-  //Build Client Object
-  $aclient = new AnnotateClient(new HttpClient(), $api_url, $api_user, $api_key);  
-    
-  //Test - List Notes
-  $noteCount = 0;
-  
-  $users = $aclient->request('listUsers');
-  foreach($users->members as $user) {
-    
-    $docs = $aclient->request('listDocuments', array(), $user);    
-    foreach($docs[0] as $doc) {
-      
-      $docinfo = $aclient->request('listNotes', array('d' => $doc->date, 'c' => $doc->code, $user));
-      echo json_format(json_encode($docinfo));
-      $noteCount++;
+    if (php_sapi_name() != 'cli') {
+        echo "<pre>";
     }
-    
-    echo "\n\n============================================================\n\n";
-  }
-  
-  echo "\nNote Count: {$noteCount}\n\n";  
+
+    //Load Config
+    $config = array();
+    if ( ! @include('config.local.php')) {
+       die("Error: Setup your configuration file (see config.sample.php for instructions)!");
+    }
+
+    $api_user = $config['api_user'];
+    $api_key  = $config['api_key'];
+    $api_url  = $config['api_url'];
+
+    //Build Client Object
+    $aclient = new AnnotateClient(new HttpClient(), $api_url, $api_user, $api_key);  
+
+    //Test - List Notes
+    $noteCount = 0;
+
+    $users = $aclient->request('listUsers');
+    foreach($users->members as $user) {
+
+      $docs = $aclient->request('listDocuments', array(), $user);    
+      foreach($docs[0] as $doc) {
+
+          $docinfo = $aclient->request('listNotes', array('d' => $doc->date, 'c' => $doc->code, $user));
+          echo json_format(json_encode($docinfo));
+          $noteCount++;
+      }
+
+      echo "\n\n============================================================\n\n";
+    }
+
+    echo "\nNote Count: {$noteCount}\n\n";  
+
+    if (php_sapi_name() != 'cli') {
+       echo "</pre>";
+    }  
+
 }
 
 // ---------------------------------------------------------------------------
